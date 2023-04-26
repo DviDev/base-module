964ef85ba02cc25c156314c331bf0c454c6817c7
Davi Menezes <davimenezes.dev@gmail.com>
2023-04-25 21:12:49 -0300

    improvements refactorings

--- Http/Livewire/BaseComponent.php
+++ Http/Livewire/BaseComponent.php
@@ -5,15 +5,59 @@ namespace Modules\Base\Http\Livewire;
 use Livewire\Component;
 use Modules\Base\Models\BaseModel;
 use Modules\LaravelProgram\Commands\DviRequestMakeCommand;
+use Modules\LaravelProgram\Domains\ViewPage;
 use Modules\LaravelProgram\Models\ViewPageModel;
 use Modules\ViewStructure\Models\ViewStructureColumnComponentModel;
+use Modules\ViewStructure\Models\ViewStructureRowModel;
 
 abstract class BaseComponent extends Component
 {
     public BaseModel $model;
     public ViewPageModel $page;
 
-    protected $visible_rows = [];
+    protected $visible_rows;
+
+    public function render()
+    {
+        $this->getRows();
+    }
+
+    public function getRows()
+    {
+        $this->page = (new ViewPage)->repository()->page(1);
+
+        $fn = function() {
+            $visible_rows = [];
+            /**@var ViewStructureRowModel $row */
+            foreach ($this->page->rows as $row) {
+                $contain = false;
+                foreach ($row->columns as $column) {
+                    /**@var ViewStructureColumnComponentModel $component */
+                    $component = $column->components->first();
+                    $contain = collect([
+                        'id',
+                        'created_at',
+                        'updated_at',
+                        'deleted_at'
+                    ])->some($component->attribute->name);
+                    if ($contain) {
+                        break;
+                    }
+
+                }
+                if ($contain) {
+                    continue;
+                }
+                $visible_rows[$row->id] = $row;
+            }
+
+            return $visible_rows;
+        };
+
+        $this->visible_rows = $this->visible_rows ?: $fn();
+
+        return $this->visible_rows;
+    }
 
     public function getRules()
     {
