c1c37d58fc3593b74229737e8859ba39605b1260
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-27 03:07:12 -0300

    improvements🔥

--- Database/Seeders/BaseDatabaseSeeder.php
+++ Database/Seeders/BaseDatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Modules\Base\Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Modules\DBMap\Database\Seeders\DBMapDatabaseSeeder;
+use Modules\ViewStructure\Database\Seeders\ViewStructureDatabaseSeeder;
 use Nwidart\Modules\Facades\Module;
 
 class BaseDatabaseSeeder extends Seeder
@@ -22,11 +24,16 @@ class BaseDatabaseSeeder extends Seeder
 
         cache()->clear();
 
-        $modules = Module::allEnabled();
+        $modules = collect(Module::allEnabled());
+
+        if ($modules->contains('DBMap') && $modules->contains('ViewStructure')) {
+            $this->call(DBMapDatabaseSeeder::class);
+            $this->call(ViewStructureDatabaseSeeder::class);
+        }
 
         /**@var \Nwidart\Modules\Laravel\Module $module */
         foreach ($modules as $module) {
-            if ($module->getName() == 'Base') {
+            if (in_array($module->getName(), ['Base', 'DBMap'])) {
                 continue;
             }
             $this->call('Modules\\' . $module->getName() . '\\Database\\Seeders\\' . $module->getName() . 'DatabaseSeeder');
