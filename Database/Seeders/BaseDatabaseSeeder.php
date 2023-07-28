cea0f5cbeb16778bbf379e4da648b1e255fdfce8
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-27 21:13:18 -0300

    🪴seeder 🔥improvements

--- Database/Seeders/BaseDatabaseSeeder.php
+++ Database/Seeders/BaseDatabaseSeeder.php
@@ -2,11 +2,16 @@
 
 namespace Modules\Base\Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Modules\App\Database\Seeders\ConfigTableSeeder;
+use Modules\App\Entities\User\UserType;
 use Modules\DBMap\Database\Seeders\DBMapDatabaseSeeder;
+use Modules\Permission\Database\Seeders\PermissionTeamsTableSeeder;
 use Modules\ViewStructure\Database\Seeders\ViewStructureDatabaseSeeder;
+use Modules\Workspace\Database\Seeders\WorkspaceTableSeeder;
 use Nwidart\Modules\Facades\Module;
 
 class BaseDatabaseSeeder extends Seeder
@@ -26,10 +31,14 @@ class BaseDatabaseSeeder extends Seeder
 
         $modules = collect(Module::allEnabled());
 
+
         if ($modules->contains('DBMap') && $modules->contains('ViewStructure')) {
             $this->call(DBMapDatabaseSeeder::class);
             $this->call(ViewStructureDatabaseSeeder::class);
         }
+        $this->call(PermissionTeamsTableSeeder::class);
+        $this->call(ConfigTableSeeder::class);
+        $this->call(WorkspaceTableSeeder::class);
 
         /**@var \Nwidart\Modules\Laravel\Module $module */
         foreach ($modules as $module) {
