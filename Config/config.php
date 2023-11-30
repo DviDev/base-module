a3ba815be27f2ec493eca7576077c52e9e64c1c4
Davi Menezes <davimenezes.dev@gmail.com>
2023-11-30 13:30:03 -0300

    improvements

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'name' => 'Base'
+    'name' => 'Base',
+    'date_format' => config('app.locale') == 'pt_BR' ? 'd/m/Y' : 'Y-m-d',
+    'date_time_format' => config('app.locale') == 'pt_BR' ? 'd/m/Y H:i' : 'Y-m-d H:i',
+    'time_format' => 'H:i:s',
 ];
