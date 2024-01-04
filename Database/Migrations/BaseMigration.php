793dd52366652697acc850159a68a1ec9f791f9a
Davi Menezes <davimenezes.dev@gmail.com>
2024-01-03 23:22:09 -0300

    improvements

--- Database/Migrations/BaseMigration.php
+++ Database/Migrations/BaseMigration.php
@@ -24,47 +24,35 @@ abstract class BaseMigration extends Migration
                 $attributeEntity = $attribute['properties'];
                 if ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::CHAR->value) {
                     $t = $table->char($attributeEntity->name, $attributeEntity->size);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::DATE->value) {
                     $t = $table->date($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::DATETIME->value) {
                     $t = $table->dateTime($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::DECIMAL->value) {
                     $size = str($attributeEntity->size)->explode(',');
                     $t = $table->decimal($attributeEntity->name, $size[0], $size[1]);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::DOUBLE->value) {
                     $t = $table->double($attributeEntity->name, $attributeEntity->size);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::FLOAT->value) {
                     $size = str($attributeEntity->size)->explode(',');
                     $t = $table->float($attributeEntity->name, $size[0], $size[1]);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::INT->value) {
                     $t = $table->integer($attributeEntity->name, $attributeEntity->size);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::MEDIUMTEXT->value) {
                     $t = $table->mediumText($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::SMALLINT->value) {
                     $t = $table->smallInteger($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::TEXT->value) {
                     $t = $table->text($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::TIME->value) {
                     $t = $table->time($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::TIMESTAMP->value) {
                     $t = $table->timestamp($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
 
                     if ($attributeEntity->use_current) {
                         $t->useCurrent();
@@ -74,14 +62,11 @@ abstract class BaseMigration extends Migration
                     }
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::TINYINT->value) {
                     $t = $table->tinyInteger($attributeEntity->name, $attributeEntity->size);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::VARCHAR->value) {
                     $t = $table->string($attributeEntity->name, $attributeEntity->size);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::YEAR->value) {
                     $t = $table->year($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::BIGINT->value) {
                     if ($attributeEntity->auto_increment) {
                         $table->id();
@@ -92,35 +77,37 @@ abstract class BaseMigration extends Migration
                             /**@var BaseModel $model */
                             $class = $relation['entity'];
                             $second_model_table_name = $class::table();
-                            $tb = $table->foreignId($attributeEntity->name)->unsigned()->references('id')->on($second_model_table_name);
+                            $t = $table->foreignId($attributeEntity->name);
+                            $fk = $t->unsigned()->references('id')->on($second_model_table_name);
                             if (!isset($relation['on_update']) || $relation['on_update'] == 'restrict') {
-                                $tb->restrictOnUpdate();
+                                $fk->restrictOnUpdate();
                             } else {
-                                $tb->cascadeOnUpdate();
+                                $fk->cascadeOnUpdate();
                             }
                             if (!isset($relation['on_delete']) || $relation['on_delete'] == 'cascade') {
-                                $tb->cascadeOnDelete();
+                                $fk->cascadeOnDelete();
                             } else {
-                                $tb->restrictOnDelete();
+                                $fk->restrictOnDelete();
                             }
                         }
-                        continue;
+                    } else {
+                        $t = $table->bigInteger($attributeEntity->name);
+                        $this->resolveUnsigned($t, $attributeEntity->unsigned);
                     }
-                    $t = $table->bigInteger($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
-                    $this->resolveUnsigned($t, $attributeEntity->unsigned);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::MEDIUMINT->value) {
                     $t = $table->mediumInteger($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required);
                     $this->resolveUnsigned($t, $attributeEntity->name);
                 } elseif ($attributeEntity->type_id == ModuleTableAttributeTypeEnum::BOOLEAN->value) {
                     $t = $table->boolean($attributeEntity->name);
-                    $t->nullable(!$attributeEntity->required)->unsigned();
+                    $t->unsigned();
                 }
                 if (!isset($t)) {
                     dd('🤖 Missing '.ModuleTableAttributeTypeEnum::from($attributeEntity->type_id)->name.' type');
                 }
-                $t->default($attributeEntity->default)->comment($attributeEntity->comments);
+                if ($attributeEntity->name == 'plano_id') {
+//                    dd($attributeEntity);
+                }
+                $t->default($attributeEntity->default)->nullable(!$attributeEntity->required)->comment($attributeEntity->comments);
             }
 
             if (isset($attributes['unique'])) {
