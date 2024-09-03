c1a22fa4787e0925e85eaacdac082982c0027428
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-03 01:50:48 -0300

    added _blank

--- Resources/views/livewire/base-form.blade.php
+++ Resources/views/livewire/base-form.blade.php
@@ -35,7 +35,7 @@
                                         builder
                                     </a>--}}
                                     @if(auth()->user()->type->enum() == UserType::DEVELOPER)
-                                        <a href="{{route('builder.page', $page->id)}}"
+                                        <a href="{{route('builder.page', $page->id)}}" target="_blank"
                                            class="bg-gray-100 text-blue-500 hover:text-blue-700 border border-gray-200 rounded-l px-2 py-1">
                                             <i class="fas fa-cogs"></i>
                                             builder
