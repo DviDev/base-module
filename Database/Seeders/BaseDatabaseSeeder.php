79cd40c3d7c6cd2131a6c0e1576a1c75a8d53db6
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-05 05:44:36 -0300

    base seeder separator with 2 seeders. Person code between 2 seeders.

--- Database/Seeders/BaseDatabaseSeeder.php
+++ Database/Seeders/BaseDatabaseSeeder.php
@@ -4,13 +4,6 @@ namespace Modules\Base\Database\Seeders;
 
 use Exception;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\File;
-use Modules\App\Database\Seeders\AppDatabaseSeeder;
-use Modules\DBMap\Database\Seeders\DBMapDatabaseSeeder;
-use Modules\Permission\Database\Seeders\PermissionTeamsTableSeeder;
-use Modules\Project\Models\ElementTypeModel;
-use Modules\View\Database\Seeders\ViewDatabaseSeeder;
-use Modules\Workspace\Database\Seeders\WorkspaceTableSeeder;
 use Nwidart\Modules\Facades\Module;
 
 class BaseDatabaseSeeder extends BaseSeeder
@@ -23,63 +16,19 @@ class BaseDatabaseSeeder extends BaseSeeder
     {
         Model::unguard();
 
-        cache()->clear();
-
-        $storage_path = storage_path('app/temp_seed_files');
-        File::deleteDirectory($storage_path);
-
         $modules = collect(Module::allEnabled());
 
-        if ($modules->contains('Project')) {
-            $this->createFirstProjectElementTypes();
-        }
+        //... some code here
 
-        if ($modules->contains('DBMap')) {
-            $this->call(DBMapDatabaseSeeder::class);
-        }
+        $this->call(InitialSeeders::class, parameters: ['modules' => $modules]);
 
-        if ($modules->contains('View')) {
-            $this->call(ViewDatabaseSeeder::class);
-        }
-        if ($modules->contains('App')) {
-            $this->call(AppDatabaseSeeder::class);
-        }
         try {
-            $this->seed($modules);
+            $this->call(SecondSeeders::class);
 
             $this->commandInfo(__CLASS__, '🟢 done');
         } catch (Exception $exception) {
             $this->command->error('🤖 Error when seeding, try again.');
             throw $exception;
         }
-    }
-
-    protected function seed($modules): void
-    {
-        if ($modules->contains('Permission')) {
-            $this->call(PermissionTeamsTableSeeder::class);
-        }
-        if ($modules->contains('Workspace')) {
-            $this->call(WorkspaceTableSeeder::class);
-        }
-
-        /**@var \Nwidart\Modules\Laravel\Module $module */
-        foreach ($modules as $module) {
-            if (in_array($module->getName(), ['Base', 'App', 'DBMap'])) {
-                continue;
-            }
-            /*$scan_seeder_class = 'Modules\\' . $module->getName() . '\\Database\\Seeders\\Scan' . $module->getName() . 'ModuleSeeder';
-            if (File::exists(base_path($scan_seeder_class))) {
-                $this->call($scan_seeder_class);
-            }*/
-            $this->call('Modules\\' . $module->getName() . '\\Database\\Seeders\\' . $module->getName() . 'DatabaseSeeder');
-        }
-    }
-
-    protected function createFirstProjectElementTypes(): void
-    {
-        $this->command->info('Creating Element Type Model');
-        ElementTypeModel::query()->create(['name' => 'user type']);
-        ElementTypeModel::query()->create(['name' => 'attribute']);
     }
 }
