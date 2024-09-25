85c41dcd38d9c2f526e586724538d26e0a6264a4
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-25 20:14:03 -0300

    New translation phrase

--- Resources/lang/pt_BR/default.php
+++ Resources/lang/pt_BR/default.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'actions' => 'Ações',
-    'visible only in local environment' => 'visível apenas em ambiente local'
+    'visible only in local environment' => 'visível apenas em ambiente local',
+    'Enable any module' => 'Habilite algum modulo'
 ];
