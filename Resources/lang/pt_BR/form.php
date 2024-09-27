a5a3b8efea5586c603fd67b4ab0eb33123c9bf5f
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-27 18:40:06 -0300

    form translation

---
+++ Resources/lang/pt_BR/form.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'actions' => 'Ações',
+    'The item has been saved' => 'O item foi salvo',
+    'Saved item' => 'Item salvo',
+    'visible only in local environment' => 'visível apenas em ambiente local',
+    'Enable any module' => 'Habilite algum modulo'
+];
