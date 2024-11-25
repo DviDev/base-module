f6560036d80d2f74a170660c5b94d51f813aa685
Davi Menezes <davimenezes.dev@gmail.com>
2024-11-24 22:08:16 -0300

    refactorings

--- Events/BaseSeederInitialIndependentDataEvent.php
+++ Events/BaseSeederInitialIndependentDataEvent.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Base\Events;
 
-use Illuminate\Console\Command;
 use Illuminate\Queue\SerializesModels;
 
 class BaseSeederInitialIndependentDataEvent
@@ -12,7 +11,7 @@ class BaseSeederInitialIndependentDataEvent
     /**
      * Create a new event instance.
      */
-    public function __construct(public Command $command)
+    public function __construct()
     {
         //
     }
