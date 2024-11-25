e8ccf0377f5f51f6fdeadbaf7bc1dc40d41c68e7
Davi Menezes <davimenezes.dev@gmail.com>
2024-11-24 22:05:33 -0300

    dispatch events moved to commands

--- Database/Seeders/BaseDatabaseSeeder.php
+++ Database/Seeders/BaseDatabaseSeeder.php
@@ -5,10 +5,6 @@ namespace Modules\Base\Database\Seeders;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\File;
-use Modules\Base\Events\BaseSeederInitialIndependentDataEvent;
-use Modules\Base\Events\DatabaseSeederEvent;
-use Modules\Base\Events\SeederFinishedEvent;
-use Modules\DBMap\Events\ScanTableEvent;
 
 class BaseDatabaseSeeder extends BaseSeeder
 {
@@ -24,11 +20,6 @@ class BaseDatabaseSeeder extends BaseSeeder
 
         $storage_path = storage_path('app/temp_seed_files');
         File::deleteDirectory($storage_path);
-
-        event(new BaseSeederInitialIndependentDataEvent($this->command));
-        event(new ScanTableEvent($this->command));
-        event(new DatabaseSeederEvent($this->command));
-        event(new SeederFinishedEvent($this->command));
 
         $this->commandInfo(__CLASS__, '🟢 done');
     }
