fbb224832e6218f9ca1481a0616a5f2df640524f
Davi Menezes <davimenezes.dev@gmail.com>
2024-12-02 20:33:25 -0300

    notification components improvements

--- Resources/views/livewire/notification/notification-list.blade.php
+++ Resources/views/livewire/notification/notification-list.blade.php
@@ -1,49 +1,40 @@
-<x-lte::card card_id="notifications" :header="str(__('notifications'))->ucfirst()" class="shadow-none border-0">
-    <x-lte::card.body>
-        <table class="table table-sm">
-            <thead>
-            <tr>
-                <th>Notificações</th>
-                <th>Data</th>
-                <th style="width: 10%"></th>
-                <th style="width: 20%"></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($notifications = $this->notifications() as $notification)
-                <tr>
-                    <td class="text-sm">
-                        <div>{{$notification->data['title']}}</div>
-                        <div class="text-gray-400">{{str($notification->data['description'])->limit(60)}}</div>
-                    </td>
-                    <td class="h-full">
-                        <div class="flex justify-content-start items-center">
-                            <span>{{(new \Carbon\Carbon($notification->created_at))->format('Y-m-d H:i')}}</span>
-                        </div>
-                    </td>
-                    <td>
-                        <i @class([
-                            "fas fa-1x",
-                             "fa-eye text-success" => $notification->read_at,
-                             "fa-eye-slash text-warning" => !$notification->read_at,
-                             ])
-                           title="{{$notification->read_at ? 'lido' : 'não lido'}}"
-                        ></i>
-                    </td>
-                    <td>
-                        <a href="{{route('notification', $notification->id)}}"
-                           class="btn btn-outline-primary btn-sm mt-1">
-                            <i class="fas fa-eye"></i> Detalhes
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </x-lte::card.body>
-    @if($notifications->total() > $notifications->perPage())
-        <x-lte::card.footer class="flex">
-            {{$notifications->links()}}
-        </x-lte::card.footer>
-    @endif
-</x-lte::card>
+<table class="table table-auto w-full overflow-auto">
+    <thead>
+    <tr>
+        <th>Notificações</th>
+        <th>Data</th>
+        <th style="width: 10%"></th>
+        <th style="width: 20%"></th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($this->notifications() as $notification)
+        <tr @class(["rounded-lg","bg-gray-100 dark:bg-gray-700" => $loop->odd])>
+            <td @class(["text-center", "rounded-bl-lg" => $loop->last])>
+                <div>{{$notification->data['title']}}</div>
+                <div class="text-gray-400">{{str($notification->data['description'])->limit(60)}}</div>
+            </td>
+            <td class="h-full">
+                <div class="flex justify-content-start items-center">
+                    <span>{{(new \Carbon\Carbon($notification->created_at))->format('Y-m-d H:i')}}</span>
+                </div>
+            </td>
+            <td>
+                <i @class([
+                    "fas fa-1x",
+                     "fa-eye text-green-600" => $notification->read_at,
+                     "fa-eye-slash text-orange-600" => !$notification->read_at,
+                 ])
+                   title="{{$notification->read_at ? 'lido' : 'não lido'}}"
+                ></i>
+            </td>
+            <td @class(["px-2 py-1","rounded-br-lg" => $loop->last])>
+                <a href="{{route('notification', $notification->id)}}"
+                   class="mt-1">
+                    <x-flowbite::button :light="true" label="Detalhes" class="w-25 rounded text-sky-500"/>
+                </a>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
--- Resources/views/livewire/notification/notification-view.blade.php
+++ Resources/views/livewire/notification/notification-view.blade.php
@@ -1,44 +1,38 @@
-<x-lte::card card_id="notificação" class="shadow-none border-0">
-    <x-lte::card.body>
-        <label>Conteúdo</label>
+<diiv>
+
+    <label @class(['font-bold text-gray-700'])>Conteúdo</label>
+    <div class="mb-2">
+        {{$notification->data['title']}}
+    </div>
+    <label @class(['font-bold text-gray-700'])>Descrição</label>
+    <div class="mb-2">
+        {!! nl2br(e($notification->data['description'])) !!}
+    </div>
+    @if($user)
+        <label @class(['font-bold text-gray-700'])>User</label>
         <div class="mb-2">
-            {{$notification->data['title']}}
+            @if($user->id == auth()->user()->id)
+                {{$user->name}}
+            @elseif( ($user?->isAdmin()))
+                Administrador
+            @endif
         </div>
-        <label>Descrição</label>
-        <div class="mb-2">
-            {!! nl2br(e($notification->data['description'])) !!}
+    @endif
+    <label @class(['font-bold text-gray-700'])>Data</label>
+    <div class="mb-2">
+        <div>{{(new \Carbon\Carbon($notification->created_at))->format('d/m/Y H:i')}}</div>
+    </div>
+    @if($action =  $this->getAction())
+        <div class="my-4">
+            <x-dvui::link md
+                          :url="$action->url" :text="$action->text"
+                          :btn="$action->btn"
+                          :primary="$action->type == 'info'"
+                          :success="$action->type == 'success'"
+                          :warning="$action->type == 'warning'"
+                          :danger="$action->type == 'danger'"
+                          :secondary="!$action->type"
+            />
         </div>
-        @if($user)
-            <label>User</label>
-            <div class="mb-2">
-                @if($user->id == auth()->user()->id)
-                    {{$user->name}}
-                @elseif( ($user?->isAdmin()))
-                    Administrador
-                @endif
-            </div>
-        @endif
-        <label>Data</label>
-        <div class="mb-2">
-            <div>{{(new \Carbon\Carbon($notification->created_at))->format('d/m/Y H:i')}}</div>
-        </div>
-        @if($action =  $this->getAction())
-            <div class="mt-2">
-                <x-dvui::link md
-                              :url="$action->url" :text="$action->text"
-                              :btn="$action->btn"
-                              :primary="$action->type == 'info'"
-                              :success="$action->type == 'success'"
-                              :warning="$action->type == 'warning'"
-                              :danger="$action->type == 'danger'"
-                              :secondary="!$action->type"
-                />
-            </div>
-        @endif
-    </x-lte::card.body>
-    <x-lte::card.footer>
-        <a href="{{url()->previous()}}" class="btn btn-default">
-            <i class="fas fa-arrow-left"></i> Voltar
-        </a>
-    </x-lte::card.footer>
-</x-lte::card>
+    @endif
+</diiv>
