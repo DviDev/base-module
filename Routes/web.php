51d032eb59809ee8dd06fa2058a1a678f25f92fa
Davi Menezes <davimenezes.dev@gmail.com>
2024-12-02 20:33:52 -0300

    base routes improvements

--- Routes/web.php
+++ Routes/web.php
@@ -13,17 +13,26 @@
 
 use Illuminate\Notifications\DatabaseNotification;
 use Modules\Base\Models\ConfigModel;
+use Modules\DvUi\View\BaseBladeComponent;
 
-Route::prefix('admin/app')->middleware(['auth', 'verified'])->group(function () {
+Route::prefix('admin/')->middleware(['auth', 'verified'])->group(function () {
     Route::get('config/list', fn() => view('lte::components.pages.config.config_list_page'))
         ->name('admin.configs');
     Route::get('config/{config?}', fn(ConfigModel $config) => view('lte::components.pages.config.config_form_page', compact('config')))
         ->name('admin.config');
 
-    Route::get('notifications', fn() => view('base::page.notification.notification-list-page'))
+    Route::get('notifications', function () {
+        if (BaseBladeComponent::published('base.page.notification.notification-list-page')) {
+            return view('components.base.page.notification.notification-list-page');
+        }
+        return view('base::components.page.notification.notification-list-page');
+    })
         ->name('my-notifications');
 
     Route::get('notification/{notification}', function (DatabaseNotification $notification) {
-        return view('base::page.notification.notification-view-page', compact('notification'));
+        if (BaseBladeComponent::published('base.page.notification.notification-view-page')) {
+            return view('components.base.page.notification.notification-view-page', compact('notification'));
+        }
+        return view('base::components.page.notification.notification-view-page', compact('notification'));
     })->name('notification');
 });
