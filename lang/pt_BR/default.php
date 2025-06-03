e1ed5e0b65fc72923a52c7c7e21af6129a82d606
Davi Menezes <davimenezes.dev@gmail.com>
2025-06-03 18:35:48 -0300

    land directory moved from resources/lang to lang

---
+++ lang/pt_BR/default.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'actions' => 'Ações',
+    'visible only in local environment' => 'visível apenas em ambiente local',
+    'Enable any module' => 'Habilite algum modulo',
+    'name' => 'nome',
+    'password' => 'Senha',
+    'password confirmation' => 'Confirmação de senha',
+    'confirm password' => 'Confirme a senha',
+    "back" => "voltar",
+];
