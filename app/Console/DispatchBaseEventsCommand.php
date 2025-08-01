3a858da5bc538b5d99b07125cd6c9f2e158e0c2a
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-01 17:05:04 -0300

    dispatch DatabaseSeederEvent when DispatchBaseEventsCommand

--- app/Console/DispatchBaseEventsCommand.php
+++ app/Console/DispatchBaseEventsCommand.php
@@ -3,36 +3,25 @@
 namespace Modules\Base\Console;
 
 use Illuminate\Console\Command;
+use Modules\Base\Events\DatabaseSeederEvent;
 use Modules\Base\Events\SeederFinishedEvent;
 use Modules\DBMap\Events\ScanTableEvent;
 
 class DispatchBaseEventsCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     */
     protected $signature = 'base:dispatch_base_events';
 
-    /**
-     * The console command description.
-     */
     protected $description = 'Dispatch Base Events Command';
 
-    /**
-     * Create a new command instance.
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     */
     public function handle(): void
     {
         \Event::dispatch(new ScanTableEvent);
-        //        \Event::dispatch(new DatabaseSeederEvent());
+        \Event::dispatch(new DatabaseSeederEvent());
         \Event::dispatch(new SeederFinishedEvent);
     }
 }
