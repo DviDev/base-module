4530d3e8236736504851a9edc03fccb529aa3d70
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-21 00:13:57 -0300

    Add `DefineSearchableAttributes` listener to handle `EntityAttributesCreatedEvent` and update searchable attributes logic in `BaseEventServiceProvider`

---
+++ app/Listeners/DefineSearchableAttributes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Base\Listeners;
+
+use Modules\Base\Entities\RecordType\RecordTypeEntityModel;
+use Modules\Base\Models\RecordTypeModel;
+use Modules\Project\Events\EntityAttributesCreatedEvent;
+use Modules\Project\Models\ProjectEntityAttributeModel;
+
+class DefineSearchableAttributes
+{
+    private EntityAttributesCreatedEvent $event;
+
+    public function handle(EntityAttributesCreatedEvent $event): void
+    {
+        $this->event = $event;
+        if ($event->entity->module->name !== config('base.name')) {
+            return;
+        }
+
+        foreach ($event->entity->entityAttributes as $attribute) {
+            $this->default($attribute);
+        }
+    }
+
+    protected function default(ProjectEntityAttributeModel $attribute): void
+    {
+        if ($this->event->entity->name !== RecordTypeModel::table()) {
+            return;
+        }
+        $p = RecordTypeEntityModel::props();
+        if (in_array($attribute->name, [
+            $p->id,
+        ])) {
+            $attribute->update(['searchable' => true]);
+        }
+    }
+}
