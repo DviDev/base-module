12f01e6b7ff4fc49d1cae4fdf1e8bd110cbd3caa
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-29 12:17:27 -0300

    Refactor `HasFactoryFirstOrCreate` trait: add exception import, type declaration, and update syntax for consistency.

--- app/Traits/HasFactoryFirstOrCreate.php
+++ app/Traits/HasFactoryFirstOrCreate.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Base\Traits;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -13,14 +14,15 @@ trait HasFactoryFirstOrCreate
      * Find the first record matching the attributes or create it using the factory.
      *
      * @return static
+     * @throws Exception
      */
-    public static function firstOrCreateViaFactory(array $attributes, array $values = [])
+    public static function firstOrCreateViaFactory(array $attributes, array $values = []): static
     {
         if (app()->isProduction()) {
-            throw new \Exception(__('This method is not allowed in production'));
+            throw new Exception(__('This method is not allowed in production'));
         }
 
-        if (! is_null($instance = static::where($attributes)->first())) {
+        if (!is_null($instance = static::where($attributes)->first())) {
             return $instance;
         }
 
